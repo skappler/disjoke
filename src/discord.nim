0cd1707af7dfb83db1cc287a45e0e4a5b7e89603
ire4ever1190 <37161@stpats.vic.edu.au>
2020-08-17 19:57:17 +1000

    Added more documentation, fixed postStats
    post stats can now be used by anyone

--- src/discord.nim
+++ src/discord.nim
@@ -270,6 +270,8 @@ macro genCommandStructure*(body: untyped): untyped =
     echo(astGenRepr(result))
 
 proc postStats*(r: Ready) {.async.} =
+    # Updates the bots count on top.gg
+    # TODO move to a different file
     when declared(DISCORD_BOTS_TOKEN):
         Info.echo("Updating Stats")
         let client = newAsyncHttpClient()
@@ -281,7 +283,7 @@ proc postStats*(r: Ready) {.async.} =
         let serverCount = r.guilds.len()
         Debug.echo($serverCount)
         let requestBody = %*{"server_count": $serverCount}
-        asyncCheck client.request("https://top.gg/api/bots/481737806599946242/stats", httpMethod=HttpPost, body = $requestBody)
+        asyncCheck client.request(fmt"https://top.gg/api/bots/{r.user.id}/stats", httpMethod=HttpPost, body = $requestBody)
 
 proc updateStatus(s: Shard) {.async.} =
     while true:
@@ -298,7 +300,7 @@ template Discord*(token: string, templateBody: untyped): untyped {.dirty.} =
     cl.events.on_ready = proc (s: Shard, r: Ready) {.async.} =
         # Run when bot connects to discord
         echo("Connected to Discord as " & $r.user)       
-        when defined(release):
+        when defined(release): # Only update server count if running release version
             asyncCheck r.postStats
             
         let ownerUser = await cl.api.getUser(OWNER_USER_ID)
@@ -328,7 +330,6 @@ template Discord*(token: string, templateBody: untyped): untyped {.dirty.} =
                 
             except RestError: # Catch discord errors
                 echo(getCurrentExceptionMsg())
-                echo("probably no permissions")
             except:
                 let
                     e = getCurrentException()
